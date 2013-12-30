4da0f2785cfb4c60e957cca49d68dcb26f3f3095
zhang bo <zhangbobell@163.com>
2013-12-30 17:58:26 +0800

    为启用Model模块作准备，提供了实例。

--- application/controllers/channel_scale.php
+++ application/controllers/channel_scale.php
@@ -6,7 +6,10 @@ class Channel_scale extends CI_Controller{
         $this->db->from('kpi_supplier_daily');
         $query=$this->db->get();
         var_dump($query->result());*/
-     $this->load->model('rank_database');   
+     $this->load->model('rank_database');
+     $etc_privileges = $this->rank_database->select_DB('db_sanqiang');
+     $this->load->database($etc_privileges);
+     
     var_dump( $this->rank_database->get_kpi_supplier_daily() );
     }
 }
