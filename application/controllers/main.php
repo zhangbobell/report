450f05e93b36a942e8839b363d5f68e6005166d2
zhang bo <zhangbobell@163.com>
2013-12-30 17:15:44 +0800

    增加了main控制器，和相应的用于显示的登录首页index.php。

---
+++ application/controllers/main.php
@@ -0,0 +1,28 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+    Class Main extends CI_Controller
+    {
+        public function index($page = 'index')
+        {
+
+          if ( ! file_exists('application/views/main/'.$page.'.php'))
+          {
+            // 页面不存在
+            show_404();
+          }
+
+          $data['title'] = "追灿数据决策系统";
+
+          $this->load->view('templates/header', $data);
+          $this->load->view('pages/'.$page, $data);
+          $this->load->view('templates/footer', $data);
+
+        }
+
+    }
+
---
+++ application/views/main/index.php
@@ -0,0 +1,8 @@
+<?php
+
+/* 
+ * File : index.php
+ * Author : ibm   Email: zhangbobell@163.com
+ * updateTime : 2013-12-30
+ */
+
