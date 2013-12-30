caeaa3923a53954a3e0fef22b297f8c4859c8713
zhang bo <zhangbobell@163.com>
2013-12-30 17:29:17 +0800

    使用rank_database的实例。可供参考。

--- application/controllers/channel_scale.php
+++ application/controllers/channel_scale.php
@@ -1,10 +1,12 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 class Channel_scale extends CI_Controller{
     public function index(){
-        $this->load->database('mysqli://data:data2123@192.168.1.90/db_sanqiang');
+        /*$this->load->database('mysqli://data:data2123@192.168.1.90/db_sanqiang');
         $this->db->select('id,updatetime,order_sales_fee');
         $this->db->from('kpi_supplier_daily');
         $query=$this->db->get();
-        var_dump($query->result());
+        var_dump($query->result());*/
+     $this->load->model('rank_database');   
+    var_dump( $this->rank_database->get_kpi_supplier_daily() );
     }
 }
