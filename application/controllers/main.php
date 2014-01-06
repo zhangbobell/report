9bb4a885360709eaf1663381006cb327e32a5f3b
zhang bo <zhangbobell@163.com>
2014-01-06 09:27:24 +0800

    首页的功能全部完成，注意验证码登录之后需要删除。

--- application/controllers/main.php
+++ application/controllers/main.php
@@ -11,6 +11,7 @@
             parent::__construct();
             
             $this->load->helper('captcha');
+            $this->load->library('session');
         }
 
 
@@ -22,10 +23,22 @@
             // 页面不存在
             show_404();
           }
-         /* {
-              $data['cap'] = "load captcha failed";
-          }*/
-          $vals = array(
+   
+          $data['title'] = "追灿数据决策系统";
+
+          $this->load->view('main/'.$page, $data);
+          $this->load->view('templates/footer', $data);
+
+        }
+        
+        public function get_captcha()
+        {
+            if($this->session->userdata('captcha') != "")
+            {
+                    $this->del_captcha();
+            }
+            
+            $vals = array(
               'word' => rand(1000, 10000),
               'img_path' => IMG_DIR.'/captcha/',
               'img_url' => 'http://localhost/report/public/images/captcha/',
@@ -33,16 +46,39 @@
               'img_height' => '30',
               'font_path' => PUB_DIR.'/fonts/ank.ttf'
                );
-          
-          $cap = create_captcha($vals);      
-          
-          $data['cap'] = $cap;
-          $data['title'] = "追灿数据决策系统";
 
-          $this->load->view('main/'.$page, $data);
-          $this->load->view('templates/footer', $data);
-
+            $cap = create_captcha($vals); 
+            $this->session->set_userdata('captcha',$cap['word']);
+            $this->session->set_userdata('captcha_url',$cap['time']);
+            
+            echo $cap['image'];
+        }
+        
+        public function del_captcha() 
+        {
+            $path = IMG_DIR.'/captcha/'.$this->session->userdata('captcha_url').'.jpg';
+            $this->load->helper('file');
+            unlink($path);
         }
 
+        public function validate()
+        {
+            //验证输入的验证码字段
+            if($this->input->post('captcha', TRUE) != $this->session->userdata('captcha') )
+            {
+                echo '2';
+                return;
+            }
+
+            //验证用户名密码
+            $this->load->model('rank_database');
+            $etc_privileges = $this->rank_database->select_DB('etc_privileges');
+            $this->load->database($etc_privileges);
+            
+            $this->db->where('username', $this->input->post('username', TRUE));
+            $this->db->where('passwd', $this->input->post('password', TRUE));
+            $this->db->from('user');
+            echo $this->db->count_all_results();
+        }
     }
 
---
+++ public/js/reload-and-validate.js
@@ -0,0 +1,65 @@
+/* 
+ * File : reload-and-validate.js
+ * Author : ibm   Email: zhangbobell@163.com
+ * createTime : 2013-12-31
+ */
+function get_captcha() {
+    $.get("main/get_captcha", function(data){
+        $('#captcha-image').html(data);
+    });
+};
+$(function(){
+    get_captcha();
+    $('#reload-captcha').click(get_captcha);
+});
+
+$(document).ready(function(){
+  $("#submit").click(function(event){
+          event.preventDefault();
+          
+          //验证三个输入框是否为空
+          if($.trim($("#username").val())==="")
+          {
+		$("#login_info").html("<i class=\"icon-sign-error\"></i>请填写用户名");
+                $("#username").focus();
+                return false;
+          }
+          if($.trim($("#password").val())==="")
+          {
+		$("#login_info").html("<i class=\"icon-sign-error\"></i>请填写密码");
+                $("#password").focus();
+                return false;
+          }
+          if($.trim($("#captcha-input").val())==="")
+          {
+		$("#login_info").html("<i class=\"icon-sign-error\"></i>请填写验证码");
+                $("#captcha-input").focus();
+                return false;
+          }
+          
+          //验证输入的验证码字段和用户名，密码
+          $.ajax({
+	    url:'main/validate',
+            type:'POST',
+            data:'captcha='+$('#captcha-input').val()+'username='+$('#username').val()+'&password='+$.md5($('#password').val()),
+            dataType:'json',
+            success:function(json){
+                if(json === 0)
+                {
+                    $("#login_info").html("<i class='icon-sign-error'></i>用户名密码不匹配");
+                    get_captcha();
+                    $("#captcha-input").val('');
+                }
+                else if(json === 2)
+                {
+                    $("#login_info").html("<i class='icon-sign-error'></i>验证码输入不正确");
+                    get_captcha();
+                    $("#captcha-input").val('');
+                    $("#captcha-input").focus();
+                }
+                else
+                   $("#login_info").html("<i class='icon-sign-error'></i>用户名验证正确");            
+            }
+	  });
+  });
+});
